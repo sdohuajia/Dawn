cac1720cead5083376e1265964ba20168e92a8ee
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-09-10 17:04:20 +0800

    Update Dawn.sh

--- Dawn.sh
+++ Dawn.sh
@@ -74,7 +74,7 @@ function install_and_start_dawn() {
 
     echo "$DAWNUSERNAME:$DAWNPASSWORD" > password.txt
 
-    wget -O dawn.py https://raw.githubusercontent.com/sdohuajia/Dawn/main/dawn.py
+    wget -O dawn.py https://raw.githubusercontent.com/sdohuajia/Dawn/main/dawn123.py
 
     # 启动 Dawn
     pm2 start python3 --name dawn -- dawn.py
