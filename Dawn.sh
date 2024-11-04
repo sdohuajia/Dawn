38bacc67afe9a5987be365e51b3ea63f6a876151
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-11-04 22:10:02 +0800

    Update Dawn.sh

--- Dawn.sh
+++ Dawn.sh
@@ -139,7 +139,7 @@ function setup_grassnode() {
     python3.11 -m pip install -r requirements.txt
 
     # 手动安装 httpx
-    python3.11 -m pip install httpx
+    python3.11 -m pip install httpx aiohttp
 
     # 配置代理信息
     read -p "请输入您的代理信息，格式为 http://user:pass@ip:port: " proxy_info
