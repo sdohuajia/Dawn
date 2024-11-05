9b676568f83601369312878ddd0751e811087818
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-11-05 18:13:09 +0800

    Update Dawn.sh

--- Dawn.sh
+++ Dawn.sh
@@ -165,6 +165,66 @@ function setup_grassnode() {
     read -n 1 -s -r -p "按任意键返回主菜单..."
 }
 
+# 安装和配置 Teneo 函数
+function setup_Teneonode() {
+    # 检查 teneo 目录是否存在，如果存在则删除
+    if [ -d "teneo" ]; then
+        echo "检测到 teneo 目录已存在，正在删除..."
+        rm -rf teneo
+        echo "teneo 目录已删除。"
+    fi
+    
+    # 安装 Python 3.11
+    sudo apt update
+    sudo apt install -y software-properties-common
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt update
+    # 添加 python3.11-venv 的安装
+    sudo apt install -y python3.11 python3.11-venv python3.11-dev python3-pip
+    echo "Python 3.11 和 pip 安装完成。"
+
+    echo "正在从 GitHub 克隆 grass 仓库..."
+    git clone https://github.com/sdohuajia/grass.git
+    if [ ! -d "grass" ]; then
+        echo "克隆失败，请检查网络连接或仓库地址。"
+        exit 1
+    fi
+
+    cd "grass" || { echo "无法进入 grass 目录"; exit 1; }
+
+    # 创建虚拟环境
+    python3.11 -m venv venv  # 创建虚拟环境
+    source venv/bin/activate  # 激活虚拟环境
+    
+    echo "正在安装所需的 Python 包..."
+    if [ ! -f requirements.txt ]; then
+        echo "未找到 requirements.txt 文件，无法安装依赖。"
+        exit 1
+    fi
+    
+    python3.11 -m pip install -r requirements.txt
+
+    # 手动安装 httpx
+    python3.11 -m pip install httpx 
+
+    # 配置代理信息
+    read -p "请输入您的代理信息，格式为 http://user:pass@ip:port: " proxy_info
+    proxies_file="proxies.txt"  # 修改为新的路径
+    [ ! -f "$proxies_file" ] && touch "$proxies_file"
+    { echo "$proxy_info"; cat "$proxies_file"; } > "$proxies_file.tmp" && mv "$proxies_file.tmp" "$proxies_file"
+
+    # 运行 setup.py
+    [ -f setup.py ] && { echo "正在运行 setup.py..."; python3.11 setup.py; }
+
+    echo "正在使用 screen 启动 main.py..."
+    screen -S grass python3 main.py
+    echo "使用 'screen -r grass' 命令来查看日志。"
+    echo "要退出 screen 会话，请按 Ctrl+A 然后按 D。"
+
+    # 提示用户按任意键返回主菜单
+    read -n 1 -s -r -p "按任意键返回主菜单..."
+}
+
 # 主菜单函数
 function main_menu() {
     while true; do
@@ -176,9 +236,10 @@ function main_menu() {
         echo "请选择要执行的操作:"
         echo "1. 安装部署Dawn"
         echo "2. 安装部署Grass"
-        echo "3. 退出"
+        echo "3. 安装部署Teneo"
+        echo "4. 退出"
 
-        read -p "请输入您的选择 (1，2，3): " choice
+        read -p "请输入您的选择 (1-4): " choice
         case $choice in
             1)
                 install_and_configure  # 调用安装和配置函数
@@ -187,6 +248,9 @@ function main_menu() {
                 setup_grassnode  # 调用安装和配置函数
                 ;;
             3)
+                setup_Teneonode  # 调用安装和配置函数
+                ;;    
+            4)
                 echo "退出脚本..."
                 exit 0
                 ;;
