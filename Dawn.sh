b7ede15135b08ff9a09504531de332f7bc932c98
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-11-10 22:11:23 +0800

    Update Dawn.sh

--- Dawn.sh
+++ Dawn.sh
@@ -305,6 +305,63 @@ function setup_Humanity() {
     read -n 1 -s -r -p "按任意键返回主菜单..."
 }
 
+# 安装和配置 Nodepay 函数
+function setup_Nodepay() {
+    # 检查 Nodepay 目录是否存在，如果存在则删除
+    if [ -d "Nodepay" ]; then
+        echo "检测到 Nodepay 目录已存在，正在删除..."
+        rm -rf Nodepay
+        echo "Nodepay 目录已删除。"
+    fi
+    
+    # 安装 Python 3.11
+    sudo apt update
+    sudo apt install -y software-properties-common
+    sudo add-apt-repository ppa:deadsnakes/ppa -y
+    sudo apt-get install -y python3-apt
+    # 添加 python3.11-venv 的安装
+    sudo apt install -y python3.11 python3.11-venv python3.11-dev python3-pip
+    echo "Python 3.11 和 pip 安装完成。"
+
+    echo "正在从 GitHub 克隆 Nodepay 仓库..."
+    git clone https://github.com/sdohuajia/Nodepay.git Nodepay
+    if [ ! -d "Nodepay" ]; then
+        echo "克隆失败，请检查网络连接或仓库地址。"
+        exit 1
+    fi
+
+    cd "Nodepay" || { echo "无法进入 Nodepay 目录"; exit 1; }
+
+    echo "正在安装所需的 Python 包..."
+    if [ ! -f requirements.txt ]; then
+        echo "未找到 requirements.txt 文件，无法安装依赖。"
+        exit 1
+    fi
+    
+    python3.11 -m pip install -r requirements.txt
+
+    # 手动安装 httpx
+    python3.11 -m pip install httpx 
+
+    # 配置代理信息
+    read -p "请输入您的代理信息，格式为 http://user:pass@ip:port: " proxy_info
+    proxies_file="/root/Nodepay/proxy.txtt"
+
+    # 将代理信息写入文件
+    echo "$proxy_info" > "$proxies_file"
+    echo "代理信息已添加到 $proxies_file."
+
+    echo "正在使用 tmux 启动 main.py..."
+    tmux new-session -d -s Nodepay  # 创建新的 tmux 会话，名称为 teneo
+    tmux send-keys -t teneo "cd Nodepay" C-m  # 切换到 teneo 目录
+    tmux send-keys -t teneo "python3 main.py" C-m  # 启动 main.py
+    echo "使用 'tmux attach -t Nodepay' 命令来查看日志。"
+    echo "要退出 tmux 会话，请按 Ctrl+B 然后按 D。"
+
+    # 提示用户按任意键返回主菜单
+    read -n 1 -s -r -p "按任意键返回主菜单..."
+}
+
 # 主菜单函数
 function main_menu() {
     while true; do
@@ -318,9 +375,10 @@ function main_menu() {
         echo "2. 安装部署 Grass"
         echo "3. 安装部署 Teneo"
         echo "4. Humanity每日签到"
-        echo "5. 退出"
+        echo "5. 安装部署 Nodepay"
+        echo "6. 退出"
 
-        read -p "请输入您的选择 (1,2,3,4,5): " choice
+        read -p "请输入您的选择 (1,2,3,4,5,6): " choice
         case $choice in
             1)
                 install_and_configure  # 调用安装和配置函数
@@ -335,6 +393,9 @@ function main_menu() {
                 setup_Humanity  # 调用安装和配置函数
                 ;;    
             5)
+                setup_Nodepay  # 调用安装和配置函数
+                ;;    
+            6)
                 echo "退出脚本..."
                 exit 0
                 ;;
