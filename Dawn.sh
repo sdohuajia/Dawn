bc41e4710b67cc9885ddfba2a222eaa78116af6a
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-11-12 17:22:28 +0800

    Update Dawn.sh

--- Dawn.sh
+++ Dawn.sh
@@ -87,7 +87,7 @@ function install_and_configure() {
 
     # 配置代理信息
     read -p "请输入您的代理信息，格式为 (http://user:pass@ip:port): " proxy_info
-    proxies_file="$DAWN_DIR/config/data/proxies.txt"
+    proxies_file="$DAWN_DIR/config/data/proxy.txt"
 
     # 将代理信息写入文件
     echo "$proxy_info" > "$proxies_file"
